68d48ec0e4d577200a39bdf6acf94b1ab3e4fc60
Sameer Bibikar <bibikar@utexas.edu>
2017-03-29 20:55:48 -0500

    Get colors and detect wound initial

--- healer.lua
+++ healer.lua
@@ -12,9 +12,13 @@ LATERAL_MOTOR_DIR = 1
 -- Distance definitions
 -- The total distance the lateral motor should move to move across the entire arm
 FULL_ARM_DISTANCE = 720
+FULL_ROTATION_DISTANCE = 360*20
 
 -- Speed definitions
 INJURY_SCAN_SPEED = 50
+
+-- Miscellaneous definitions
+INJURY_COLOR_THRESHOLD = 100
 
 function heal()
 	-- Using color sensor, scan entire forearm from wrist to elbow.
@@ -22,6 +26,10 @@ function heal()
 	nxt.InputSetType(COLOR_SENSOR, 13, 0x80)
 	-- 13 means to turn on all three lights
 	-- 0x80 means ???
+	--
+	-- Collect initial color values.
+	_,_,_,_,red_cali, green_cali, blue_cali = nxt.InputGetStatus(COLOR_SENSOR)
+	calibration_color = red_cali + green_cali + blue_cali
 	
 	-- We need to make sure that we are in the correct initial position.
 	-- Another assumption is that the wound is on top!
@@ -34,11 +42,23 @@ function heal()
 	nxt.OutputSetSpeed(LATERAL_MOTOR, 32, LATERAL_MOTOR_DIR*INJURY_SCAN_SPEED, FULL_ARM_DISTANCE)
 	local tacho_initial
 	_, tacho_initial = nxt.OutputGetStatus(LATERAL_MOTOR)
+	inj_tacho1
+	inj_tacho2
+	-- These are the initial and final tachometer values around the injury.
 
 	repeat
 		local tacho
 		_, tacho = nxt.OutputGetStatus(LATERAL_MOTOR)
 		-- do the color sensor stuff...
+		_,_,_,_,r,g,b = nxt.InputGetStatus(COLOR_SENSOR)
+		color = r+g+b
+		if nxt.abs(color - calibration_color) > INJURY_COLOR_THRESHOLD then
+			if inj_tacho1 ~= nil then
+				inj_tacho2 = tacho
+			else 
+				inj_tacho1 = tacho
+			end
+		end
 	until d1 - 4 <= tacho - tacho_initial
 	-- Now we are at the other end.
 	-- Move back...
@@ -57,6 +77,7 @@ function heal()
 	-- Return to the end of the wound closest to current position
 	-- Make wrap while shifting over slightly to the other side of the wound
 	-- 3 wraps over 8-10cm of wound
+	-- Here, we also do the push thing to secure the bandage?
 end
 
 heal()

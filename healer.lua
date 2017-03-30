6c33fd20c1068134acf570aed77b8b7dd5ab3b40
Sameer Bibikar <bibikar@utexas.edu>
2017-03-30 11:44:17 -0500

    First half of scraping the wound

--- healer.lua
+++ healer.lua
@@ -3,7 +3,10 @@
 -- Port definitions - make sure these are right or you might end up putting the wrong voltage into something.
 -- For motors, port A is 1, B is 2, C is 3
 COLOR_SENSOR = 4
+TOUCH_SENSOR = 1
 LATERAL_MOTOR = 1
+RING_MOTOR = 2
+RACK_MOTOR = 3
 
 -- Direction definitions - set to +1 or -1 depending on which direction everything is oriented.
 -- If something is always going backwards, switch this.
@@ -16,6 +19,7 @@ RING_MOTOR_DIR = 1
 -- The total distance the lateral motor should move to move across the entire arm
 FULL_ARM_DISTANCE = 720
 FULL_ROTATION_DISTANCE = 360*20
+INJURY_SCRAPE_THRESHOLD = 40
 
 -- Speed definitions
 INJURY_SCAN_SPEED = 50
@@ -55,6 +59,8 @@ function heal()
 		-- do the color sensor stuff...
 		_,_,_,_,r,g,b = nxt.InputGetStatus(COLOR_SENSOR)
 		color = r+g+b
+		-- TODO fix tacho2 - currently it will only find the first value and then
+		-- a value right after that.
 		if nxt.abs(color - calibration_color) > INJURY_COLOR_THRESHOLD then
 			if inj_tacho1 ~= nil then
 				inj_tacho2 = tacho
@@ -74,10 +80,37 @@ function heal()
 	nxt.OutputSetSpeed(LATERAL_MOTOR,0,0,0)
 
 	-- After a full pass, lower arm to correct height, and scrape on the return
+	nxt.OutputSetSpeed(RACK_MOTOR,0,0)
+	
+	_, tacho_initial = nxt.OutputGetStatus(LATERAL_MOTOR)
+	-- Move to the wound
+	nxt.OutputSetSpeed(LATERAL_MOTOR, 32, LATERAL_MOTOR_DIR*INJURY_SCAN_SPEED, FULL_ARM_DISTANCE)
+	repeat
+		local tacho
+		_, tacho = nxt.OutputGetStatus(LATERAL_MOTOR)
+	until tacho - inj_tacho1 < INJURY_SCRAPE_THRESHOLD
+	
+	nxt.OutputSetSpeed(LATERAL_MOTOR,0,0)
+	nxt.InputSetType(TOUCH_SENSOR,0)
+	local touchval = nxt.InputGetStatus(TOUCH_SENSOR)
+	nxt.OutputSetSpeed(RACK_MOTOR,32,-1*RACK_MOTOR_DIR*RACK_MOTOR_SPEED)
+	repeat
+		touchval = nxt.InputGetStatus(TOUCH_SENSOR)
+	until touchval < 512
+	-- touchval is < 512 if it is pressed
+	-- Lower the arm until touch sensor is pressed
+
+	-- Now we scrape the wound until inj_tacho2 + INJURY_SCRAPE_THRESHOLD
+	-- Then we lift the scraper to maximum position
+	-- TODO record initial tachometer value of rack and pinion
+
 	-- Then, reposition arm to press module against pin and drop down on the side
 	-- Reinsert connector into wrapping module
+	-- Go to center, lower rack and pinion until touch sensor is pressed. (thtat means it's clicked in)
+	-- Then stop for a bit, then lift back up with the new module attached.
 	-- Return to highest position of the arm
 	-- Return to the end of the wound closest to current position
+	-- (this is where the module holder is)
 	--
 	-- Make wrap while shifting over slightly to the other side of the wound
 	-- 3 wraps over 8-10cm of wound
